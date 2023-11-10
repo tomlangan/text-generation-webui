e7e5d52577888d6c68043ad2d1cef85ffa9023b9
Tom Langan <tom.langan@hotmail.com>
2023-11-10 04:53:25 +0000

    update deploy scripts

--- up.sh
+++ up.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-docker compose up -d --build
+
+# Stop execution if any command fails
+set -e
+
+# Define variables
+SERVICE_NAME="text-generation-webui"
+NEW_TAG="v$(date +%s)" # Using a timestamp as the version tag for simplicity
+
+git pull
+
+
+# Build the new Docker image with the new tag
+docker compose build $SERVICE_NAME
+
+# Tag the new image
+docker tag $SERVICE_NAME:latest $SERVICE_NAME:$NEW_TAG
+
+# Bring up the new containers in detached mode, without affecting the other services
+docker compose up -d --no-deps --build $SERVICE_NAME
+
+OLD_CONTAINERS=$(docker images --filter "reference=$SERVICE_NAME" --filter "before=$SERVICE_NAME:$NEW_TAG" --format "{{.Repository}}:{{.Tag}}")
+
+# Remove the old containers
+if [ -n "$OLD_CONTAINERS" ]; then
+    echo "Removing old containers..."
+    docker rmi $OLD_CONTAINERS
+fi
+
+echo "Deployment completed successfully."
 
